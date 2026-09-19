957070c739eb9305e7826cd4cd2594f360f543d6
adiantaryami7890 <136005490+adiantaryami7890@users.noreply.github.com>
2026-09-19 15:38:02 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,15 +1,34 @@
- // =========================================================
+ // ============================================================
 // SECORA — SUPABASE CLIENT
-// =========================================================
+// ============================================================
 
 const SUPABASE_URL =
   "https://iadfckatnldoruoksvtx.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-  "sb_publishable_1neR34ctfRRQut-DO3QZqA_ci07ddhF";
+  "sb_publishable_1neR34ctfRRQut-D03QZqA_ci07ddhF";
 
+
+/*
+ * Create the Supabase client.
+ */
 const secoraSupabase =
-  supabase.createClient(
+  window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_PUBLISHABLE_KEY
   );
+
+
+/*
+ * Expose the client globally.
+ *
+ * dashboard.js
+ * course.js
+ * lesson.js
+ * and other SECORA frontend files
+ * can use:
+ *
+ * window.secoraSupabase
+ */
+window.secoraSupabase =
+  secoraSupabase;
